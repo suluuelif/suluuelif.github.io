b733800ca9b66590974605d3f1ecf0e69f9b8fe2
suluuelif <esu3@uncc.edu>
2024-03-18 19:11:38 -0400

    hey

--- itis3135/scripts/survey.js
+++ itis3135/scripts/survey.js
@@ -4,6 +4,6 @@ const courseForm = document.getElementById(course-form);
 addCourseBtn.addEventListener("click", () =>
 courseForm.classList.toggle("hidden")
 );
-addCourse.addEventListener(click, function(){
-    courseForm.classList.toggle('hidden');
-})
+// addCourseBtn.addEventListener(click, function(){
+//     courseForm.classList.toggle('hidden');
+// })
