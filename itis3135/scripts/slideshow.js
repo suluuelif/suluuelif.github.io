4f7fc0feff042426ecfbcd186c34c47aef15832a
suluuelif <esu3@uncc.edu>
2024-04-28 17:04:56 -0400

    images

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,28 +1,28 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+//   }
   
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+//   function currentSlide(n) {
+//     showSlides(slideIndex = n);
+//   }
   
-  function showSlides(n) {
-    var i;
-    var slides = $(".slides"); 
-    var dots = $(".demo"); 
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+//   function showSlides(n) {
+//     var i;
+//     var slides = $(".slides"); 
+//     var dots = $(".demo"); 
+//     if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
     
-    slides.hide(); 
-    dots.removeClass("active");
+//     slides.hide(); 
+//     dots.removeClass("active");
   
-    slides.eq(slideIndex - 1).fadeIn(600); 
-    dots.eq(slideIndex - 1).addClass("active"); 
-    captionText.html(dots.eq(slideIndex - 1).attr("alt")); 
-  }
+//     slides.eq(slideIndex - 1).fadeIn(600); 
+//     dots.eq(slideIndex - 1).addClass("active"); 
+//     captionText.html(dots.eq(slideIndex - 1).attr("alt")); 
+//   }
 // function plusSlides(n) {
 //     showSlides(slideIndex += n);
 // }
@@ -43,3 +43,28 @@ function plusSlides(n) {
 //     }
 //     slides[slideIndex-1].style.display = "block";
 // }
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = $(".slides"); 
+  var dots = $(".demo"); 
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  
+  slides.hide(); 
+  dots.removeClass("active");
+
+  slides.eq(slideIndex - 1).fadeIn(600); 
+  dots.eq(slideIndex - 1).addClass("active"); 
+  captionText.html(dots.eq(slideIndex - 1).attr("alt")); 
+}
