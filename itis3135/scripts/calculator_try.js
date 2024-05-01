7caea99a303dc029c493e40e70d70c53d40c1330
suluuelif <esu3@uncc.edu>
2024-05-01 16:32:53 -0400

    calc

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -3,31 +3,30 @@ const keys = calculator.querySelector(".calculator_keys")
 const key = e.target
 const action = key.dataset.action
 
-keys.addEventListener("click", e => {
- if (e.target.matches("button")) {
-
- }
+keys.addEventListener("click", () => {
+    if (!action) {
+        console.log('number key!')
+      }
+     if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'
+      ) {
+        console.log('operator key!')
+      }
+      if (action === 'decimal') {
+        console.log('decimal key!')
+      }
+      
+      if (action === 'clear') {
+        console.log('clear key!')
+      }
+      
+      if (action === 'calculate') {
+        console.log('equal key!')
+      }
+ 
 })
 
- if (!action) {
-    console.log('number key!')
-  }
- if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-  ) {
-    console.log('operator key!')
-  }
-  if (action === 'decimal') {
-    console.log('decimal key!')
-  }
-  
-  if (action === 'clear') {
-    console.log('clear key!')
-  }
-  
-  if (action === 'calculate') {
-    console.log('equal key!')
-  }
+ 
